2f59a1595253ab82484abfb6fdea3cbbf5f0d02f
Leandro Villalba <leandrovillalba.dev>
2024-01-20 21:25:42 -0300

    ResponseEntity

--- src/main/java/edu/tienda/core/controllers/ClientRestController.java
+++ src/main/java/edu/tienda/core/controllers/ClientRestController.java
@@ -1,8 +1,11 @@
 package edu.tienda.core.controllers;
 
 import edu.tienda.core.domain.Client;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -21,26 +24,35 @@ public class ClientRestController {
         )
     );
 
-    @GetMapping
-    public List<Client> getClients() {
-        return clients;
+    @GetMapping // Creamos clase de tipo "Wrapper" que devuelve un objeto de tipo ResponseEntity
+    public ResponseEntity<?> getClients() { // "wildcard" <?> permite mayor flexibilidad ante un cambio en el cuerpo del método
+        return ResponseEntity.ok(clients);
     }
 
     @GetMapping("/{userName}")
-    public Client getClient(@PathVariable String userName) {
-        return clients.stream().
+    public ResponseEntity<?> getClient(@PathVariable String userName) {
+        return ResponseEntity.ok(clients.stream().
                 filter(client -> client.getUsername().equalsIgnoreCase(userName)).
-                findFirst().orElseThrow();
+                findFirst().orElseThrow());
     }
 
     @PostMapping
-    public Client addClient(@RequestBody Client client) {
+    public ResponseEntity<?> addClient(@RequestBody Client client) {
         clients.add(client);
-        return client;
+
+        //Obteniendo URL de servicio: retorna la URL para acceder a este nuevo recurso
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/userName")
+                .buildAndExpand(client.getUsername())
+                .toUri();
+
+
+        return ResponseEntity.created(location).body(client);
     }
 
     @PutMapping
-    public Client updateClient(@RequestBody Client client) {
+    public ResponseEntity<?> updateClient(@RequestBody Client client) {
 
         Client foundClient = clients.stream().
                 filter(cli -> cli.getUsername().equalsIgnoreCase(client.getUsername())).
@@ -49,15 +61,17 @@ public class ClientRestController {
         foundClient.setPassword(client.getPassword());
         foundClient.setName(client.getName());
 
-        return foundClient;
+        return ResponseEntity.ok(foundClient);
     }
 
     @DeleteMapping("/{userName}")
-    public void deleteClient(@PathVariable String userName) {
+    public ResponseEntity deleteClient(@PathVariable String userName) {
         Client foundClient = clients.stream().
                 filter(cli-> cli.getUsername().equalsIgnoreCase(userName)).
                 findFirst().orElseThrow();
         clients.remove(foundClient);
+
+        return ResponseEntity.noContent().build();
     }
 
 }
